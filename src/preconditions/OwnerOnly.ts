58dc146da95e40e372e028233646a099a81cf2c7
David Gustavo Herrera De La Cruz <31642521+Phamzito@users.noreply.github.com>
2020-09-14 11:08:16 -0500

    feat: translate preconditions (#2)
    * feat(lang): translate preconditions
    
    * refactor: remove CommandOptions

--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,9 +1,11 @@
 import { OWNER_ID } from '@root/config';
-import { Precondition, Awaited, Result, UserError, ok, err } from '@sapphire/framework';
+import { Precondition, UserError, ok, err } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
-export class OwnerOnly extends Precondition {
-	public run(message: Message): Awaited<Result<unknown, UserError>> {
-		return message.author.id === OWNER_ID ? ok() : err(new UserError('ownerOnly', 'Este comando solo puede ser usado por el dueño.'));
+export class DasbyPrecondition extends Precondition {
+	public async run(message: Message) {
+		return message.author.id === OWNER_ID
+			? ok()
+			: err(new UserError(this.name, await message.fetchLanguageKey('preconditions/owneronly:errorMessage')));
 	}
 }
