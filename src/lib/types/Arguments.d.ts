9fcb467286d595faf30495640a04a23fb7e7cc6a
Phamzito <phamzito@hotmail.com>
2020-09-15 20:11:01 -0500

    aaaah...

--- src/lib/types/Arguments.d.ts
+++ src/lib/types/Arguments.d.ts
@@ -1,9 +1,6 @@
 import type { User, Emoji } from 'discord.js';
 
 declare module '@sapphire/framework' {
-	interface CommandOptions {
-		category?: string;
-	}
 	interface ArgType {
 		user: User;
 		boolean: boolean;
