2e867081938e0164589943532db52524c4bc2425
Phamzito <phamzito@hotmail.com>
2020-09-15 20:09:38 -0500

    chore: new arguments

--- src/lib/types/Arguments.d.ts
+++ src/lib/types/Arguments.d.ts
@@ -1,6 +1,9 @@
 import type { User, Emoji } from 'discord.js';
 
 declare module '@sapphire/framework' {
+	interface CommandOptions {
+		category?: string;
+	}
 	interface ArgType {
 		user: User;
 		boolean: boolean;
